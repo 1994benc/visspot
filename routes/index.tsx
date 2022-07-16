de3e061bb548d7c509227496945ddb39a12b137c
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2022-07-16 10:51:08 +0100

    add github

--- routes/index.tsx
+++ routes/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             </div>
           </li>
         </ul>
-        <div class={`flex gap-2`}>
+        <div class={tw`flex gap-2`}>
           <a href="https://form.jotform.com/221962077087058" target="blank">
             <Button>
               <div class={tw`flex items-center gap-2`}>
@@ -56,6 +56,27 @@ export default function Home() {
                   />
                 </svg>
                 <div>Register your interest</div>
+              </div>
+            </Button>
+          </a>
+          <a href="https://github.com/1994benc/VisSpot.com" target="blank">
+            <Button type={ButtonType.Outline}>
+              <div class={tw`flex items-center gap-2`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class={tw("h-6 w-6")}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
+                  />
+                </svg>
+                <div>Github</div>
               </div>
             </Button>
           </a>
